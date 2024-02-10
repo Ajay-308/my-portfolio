e6595d07408cf87ec5d2377666e031024c23bcc9
Ajay <112794893+Ajay-308@users.noreply.github.com>
2024-02-10 19:20:10 +0530

    page.tsx fix

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
         style={{ backgroundImage: "url(/main-bg.webp)" }}
       >
         <div className="pl-20 md:pl-40 pb-56 md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px]">
-          <h1 className="text-[50px] text-white font-semibold">
-            Hi <img src="https://raw.githubusercontent.com/MartinHeinz/MartinHeinz/master/wave.gif" width="30px">,  I am Ajay Singh <br />{" "}
+          <h1 className="text-[50px]  text-white font-semibold">
+            Hi there, I am Ajay <br />{" "}
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500">
               {" "}
               FUll Stack Developer{""}
