fa64a97ec8fac9ea324b6a0b969d7281a399ed48
Ajay <112794893+Ajay-308@users.noreply.github.com>
2024-02-10 19:14:28 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       >
         <div className="pl-20 md:pl-40 pb-56 md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px]">
           <h1 className="text-[50px] text-white font-semibold">
-            Hello I am Ajay, a <br />{" "}
+            Hi <img src="https://raw.githubusercontent.com/MartinHeinz/MartinHeinz/master/wave.gif" width="30px">,  I am Ajay Singh <br />{" "}
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500">
               {" "}
               FUll Stack Developer{""}
