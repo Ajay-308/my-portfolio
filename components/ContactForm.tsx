e861ff4a92279827b6bb41c64aaec1488cd3de8c
Ajay <112794893+Ajay-308@users.noreply.github.com>
2024-02-16 11:31:49 +0530

    visme.com

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,55 +1,30 @@
-import React from "react";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Textarea } from "./ui/textarea";
+"use client";
+import React, { useEffect } from "react";
 
 const ContactForm = () => {
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      const script = document.createElement("script");
+      script.src = "https://static-bundles.visme.co/forms/vismeforms-embed.js";
+      script.defer = true;
+      document.body.appendChild(script);
+    }, 100); // Adjust the timeout as needed
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+
   return (
     <section className="contact mr-12 -ml-12" id="contact">
-      <h2 className="font-bold text-white ml-12 cursive ">
-        Contact <span>Me</span>
-      </h2>
-      <form
-        action="https://formspree.io/f/xoqzpanb"
-        method="POST"
-        className="contact-form"
-      >
-        <label className="input-box">
-          <Input
-            className="mt-2 mix-blend-soft-light w-96"
-            type="text"
-            name="name"
-            placeholder="Your name"
-          />
-          <Input
-            className="mt-2 mix-blend-soft-light w-96"
-            type="email"
-            name="email"
-            placeholder="Your email"
-          />
-        </label>
-        <label className="input-box">
-          <Input
-            className="mt-2 mix-blend-soft-light w-96"
-            type="number"
-            name="number"
-            placeholder="Your number"
-          />
-          <Textarea
-            className="mt-2 mix-blend-soft-light w-96"
-            name="message"
-            placeholder="Send your query"
-          ></Textarea>
-        </label>
-        {/* Your other form fields go here */}
-        <Button
-          type="submit"
-          className="bg-gradient-to-br from-pink-500 to-violet-400 mix-blend-soft-light font-bold cursor-pointer hover:bg-white rounded-full h-8 w-32 mt-4 text-black"
-          value="Send message"
-        >
-          send
-        </Button>
-      </form>
+      <div
+        className="visme_d"
+        data-title="Untitled Proj"
+        data-url="90xzgyz6-untitled-proj?fullPage=true"
+        data-domain="forms"
+        data-full-page="true"
+        data-min-height="100vh"
+        data-form-id="15546"
+        style={{ background: "transparent" }}
+      ></div>
     </section>
   );
 };
