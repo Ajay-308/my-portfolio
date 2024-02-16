5725a374f59febef237dd3384f57b4a1b8b06693
Ajay <112794893+Ajay-308@users.noreply.github.com>
2024-02-16 11:56:38 +0530

    visme.com

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -8,13 +8,14 @@ const ContactForm = () => {
       script.src = "https://static-bundles.visme.co/forms/vismeforms-embed.js";
       script.defer = true;
       document.body.appendChild(script);
-    }, 100); // Adjust the timeout as needed
+    }, 10);
 
     return () => clearTimeout(timeoutId);
   }, []);
 
   return (
     <section className="contact mr-12 -ml-12" id="contact">
+      <h1 className="mt-4 ml-4 font-bold text-3xl">Please Wait ...</h1>
       <div
         className="visme_d"
         data-title="Untitled Proj"
@@ -23,7 +24,7 @@ const ContactForm = () => {
         data-full-page="true"
         data-min-height="100vh"
         data-form-id="15546"
-        style={{ background: "transparent" }}
+        style={{ backgroundImage: "url(bg-3.jpg)" }}
       ></div>
     </section>
   );
