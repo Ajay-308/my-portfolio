a0478247bb7bcc1d204cff30ccb1fa2c51df6318
Ajay <112794893+Ajay-308@users.noreply.github.com>
2024-07-12 19:35:18 +0530

    fixed error

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Navigation from "@/components/Navigation";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Navigation />
+        <Toaster />
       </body>
     </html>
   );
--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,35 +1,157 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useState, ChangeEvent, FormEvent } from "react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
+import { Form } from "./ui/form";
+import { Label } from "./ui/label";
+import toast, { Toaster } from "react-hot-toast";
+const Contact: React.FC = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    number: "",
+    message: "",
+  });
 
-const ContactForm = () => {
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      const script = document.createElement("script");
-      script.src = "https://static-bundles.visme.co/forms/vismeforms-embed.js";
-      script.defer = true;
-      document.body.appendChild(script);
-    }, 10);
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
 
-    return () => clearTimeout(timeoutId);
-  }, []);
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (
+      formData.name === "" ||
+      formData.email === "" ||
+      formData.number === "" ||
+      formData.message === ""
+    ) {
+      toast.error("please fill all the fields");
+      return;
+    }
+    if (formData.number.length !== 10) {
+      toast.error("please enter a valid number");
+      return;
+    }
+
+    fetch("https://formspree.io/f/xoqzpanb", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => {
+        if (response.ok) {
+          toast.success("message sent successfully!");
+          setFormData({
+            name: "",
+            email: "",
+            number: "",
+            message: "",
+          });
+        } else {
+          toast.error("failed to send message");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        toast.error("an error occured while sending the message");
+      });
+  };
 
   return (
-    <section className="contact mr-12 -ml-12" id="contact">
-      <h1 className="mt-4 ml-4 font-bold text-3xl">
-        Please touch screen once and Wait ...
-      </h1>
-      <div
-        className="visme_d"
-        data-title="Untitled Proj"
-        data-url="90xzgyz6-untitled-proj?fullPage=true"
-        data-domain="forms"
-        data-full-page="true"
-        data-min-height="100vh" 
-        data-form-id="15546"
-        style={{ backgroundImage: "url(bg-3.jpg)" }}
-      ></div>
+    <section className="contact" id="contact">
+      <Toaster />
+      <h2 style={{ color: "white" }} className="heading">
+        Contact <span>Me</span>
+      </h2>
+      <form onSubmit={handleSubmit}>
+        <Label className="input-box">
+          <Input
+            type="text"
+            name="name"
+            style={{
+              background: "transparent",
+              color: "white",
+              width: "25rem",
+              marginLeft: "-10rem",
+              height: "3rem",
+            }}
+            placeholder="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+          <Input
+            type="email"
+            name="email"
+            style={{
+              background: "transparent",
+              color: "white",
+              width: "25rem",
+              marginLeft: "-10rem",
+              height: "3rem",
+            }}
+            placeholder="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </Label>
+        <Label className="input-box">
+          <Input
+            type="number"
+            name="number"
+            style={{
+              background: "transparent",
+              color: "white",
+              width: "25rem",
+              marginLeft: "-10rem",
+              height: "3rem",
+            }}
+            placeholder="number"
+            value={formData.number}
+            onChange={handleChange}
+          />
+          <Textarea
+            name="message"
+            placeholder="send your query"
+            className="send"
+            style={{
+              background: "transparent",
+              color: "white",
+              width: "25rem",
+              border: "white",
+              marginLeft: "-10rem",
+              height: "7rem",
+            }}
+            value={formData.message}
+            onChange={handleChange}
+          />
+        </Label>
+        <Button
+          style={{
+            color: "white",
+            width: "13rem",
+            background: "#3B82F6",
+            border: "white",
+            marginLeft: "-5rem",
+          }}
+          type="submit"
+          title="Send"
+          className="btn"
+        >
+          Send Message
+        </Button>
+      </form>
     </section>
   );
 };
 
-export default ContactForm;
+export default Contact;
