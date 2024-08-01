e859282b9cdd9fc5d85134d151e47200e61e7bec
Ajay <112794893+Ajay-308@users.noreply.github.com>
2024-08-02 03:29:35 +0530

    final for today

--- app/contact-me/page.tsx
+++ app/contact-me/page.tsx
@@ -8,7 +8,7 @@ const Page = () => {
       className="xl:w-screen lg:w-screen w-[100vw] xl:h-screen lg:h-screen h-[130vh] bg-cover bg-center flex items-center justify-center"
     >
       <div className="h-[60%] w-[80%] max-w-screen-md relative flex items-center justify-center rounded-xl">
-        <div className="w-full md:w-[100%] xl:mr-[30rem] lg:w-[30%] ">
+        <div className="w-full md:w-[100%]  lg:w-[30%] ">
           <ContactForm />
         </div>
       </div>
--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import { motion } from "framer-motion";
+import { PinContainer } from "./ui/3d-pin";
 
 interface Props {
   image: string;
@@ -19,27 +20,30 @@ const ProjectCard = ({ image, title, text }: Props) => {
     }
   }
   return (
-    <div
-      onClick={handleFlip}
-      className="w-[450px] h-[280px] rounded-md cursor-pointer"
-    >
-      <motion.div
-        className="flip-card-inner w-full h-full"
-        initial={false}
-        animate={{ rotateY: isFlipped ? 180 : 360 }}
-        transition={{ duration: 0.6, animationDirection: "normal" }}
-        onAnimationComplete={() => setIsAnimating(false)}
+    <div className="flex flex-wrap justify-center gap-4 p-4">
+      <PinContainer
+        title={title}
+        className=" w-[30rem] sm:w-[450px] h-[280px] rounded-md cursor-pointer"
       >
-        <div
-          style={{ backgroundImage: `url(${image})` }}
-          className="w-full h-full group relative flip-card-front bg-cover bg-center text-white rounded-lg p-4"
+        <motion.div
+          className="flip-card-inner w-full h-full"
+          initial={false}
+          animate={{ rotateY: isFlipped ? 180 : 360 }}
+          transition={{ duration: 0.6, animationDirection: "normal" }}
+          onAnimationComplete={() => setIsAnimating(false)}
         >
-          <div className="absolute inset-0 w-full h-full rounded-md bg-black opacity-0 group-hover:opacity-40" />
-          <div className="absolute inset-0 w-full h-full text-[20px] pb-10 hidden group-hover:flex items-center z-[20] justify-center">
-            Learn more &gt;
+          <div
+            style={{ backgroundImage: `url(${image})` }}
+            className="w-full h-full group relative flip-card-front bg-cover bg-center text-white rounded-lg p-4"
+          >
+            <div className="absolute inset-0 w-full h-full rounded-md bg-black opacity-0 group-hover:opacity-40" />
+            <div className="absolute inset-0 w-full h-full text-[20px] pb-10 hidden group-hover:flex items-center z-[20] justify-center">
+              Learn more &gt;
+            </div>
           </div>
-        </div>
-      </motion.div>
+        </motion.div>
+      </PinContainer>
+      {/* Add more PinContainer components here as needed */}
     </div>
   );
 };
